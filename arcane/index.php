7695d220efa84a4a9911d4eaa8529fe2dfb24063
Anupriya-Singh <anupriya228@gmail.com>
2017-09-25 02:15:41 +0530

    Home page, Instructions

---
+++ arcane/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Arcane'17</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+<body>
+	<div class = "home">
+		<div class = "tophead">
+			Innerve-IGDTUW presents
+		</div>
+	
+		<div class = "container">
+			<div class = "heading">
+				Arcane 2k17
+			</div>
+			<div class = "subheading">
+				- online cryptic hunt
+			</div>
+			<button class = "button">
+				<a href="log.php">Enter the hunt!</a>
+			</button>
+		</div>
+	</div>
+</body>
+</html>
---
+++ arcane/rules.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Instructions</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+<body>
+	<div class = "instructions">
+	<div class = "title">Instructions</div>
+		<div class = "rules">
+			<i>Read the instructions carefully before starting the hunt:</i>
+			<ul>
+				<li>The game play involves getting into the next level after submitting the appropriate answer at each level.
+				<li>Submit the answers in lower case.
+				<li>If the answer is of more than 2 words, space should be provided between the words.
+					<br>For example: hello world
+				<li>Google answer is allowed.
+				<li>Look for the hints on the webpage, the source code and the URL.
+				<li>There is no restriction on the number of attempts.
+				<li>Hints will be provided at our facebook page, fblink.
+			</ul>
+		</div>
+		<button class = "button">
+			<a href = "ques1.php">Start!</a>
+		</button>
+	</div>
+</body>
+</html>
