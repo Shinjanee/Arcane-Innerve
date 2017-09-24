35980b56f0e6aeb2ee53bf15c79e64dbf0a97be5
Anupriya-Singh <anupriya228@gmail.com>
2017-09-25 02:07:47 +0530

    Update ques19.php

--- arcane/ques19.php
+++ arcane/ques19.php
@@ -22,6 +22,28 @@
         }
 	};
 	</script>
+	
+	<script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+    <script type="text/javascript" charset="utf-8">
+	document.onkeydown = function(){
+  switch (event.keyCode){
+        case 116 : //F5 button
+            event.returnValue = false;
+            event.keyCode = 0;
+            return false;
+        case 82 : //R button
+            if (event.ctrlKey){ 
+                event.returnValue = false;
+                event.keyCode = 0;
+                return false;
+            }
+    }
+}
+
+	window.location.hash="no-back-button";
+window.location.hash="Again-No-back-button";//again because google chrome don't insert first hash into history
+window.onhashchange=function(){window.location.hash="no-back-button";}
+</script>
 	<div class="title">
 		<!-- <h1> Question 1: </h1> -->
 	</div>
