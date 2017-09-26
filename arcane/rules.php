49c9dfe411ce059f17e29dab93e41e5fbecc6ec8
Anupriya-Singh <anupriya228@gmail.com>
2017-09-27 02:43:46 +0530

    Update rules.php

--- arcane/rules.php
+++ arcane/rules.php
@@ -12,9 +12,10 @@
 			<ul>
 				<li>The game play involves getting into the next level after submitting the appropriate answer at each level.<br><br>
 				<li>Submit the answers in lower case.<br><br>
-				<li>If the answer is of more than 2 words, space should be provided between the words.<br>
-					For example: hello world<br><br>
-				<li>Google answer is allowed.<br><br>
+				<li>No space allowed.<br><br>
+				<li>Apostrophe allowed.<br>
+					For example: rohan'sdog<br><br>
+				<li>Google search is allowed.<br><br>
 				<li>Look for the hints on the webpage, the source code and the URL.<br><br>
 				<li>There is no restriction on the number of attempts.<br><br>
 				<li>Hints will be provided at our facebook page, fblink.
