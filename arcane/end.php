d5a4ba994c723fc1147b4721064d8dee6b3d51ad
Anupriya-Singh <anupriya228@gmail.com>
2017-09-27 23:37:22 +0530

    Last page(After completing all levels)

---
+++ arcane/end.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Congratulations</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+	<div class="congo">
+		<h1>Congratulations!</h1><br>
+		You have completed all the levels.
+	</div>
+</html>
