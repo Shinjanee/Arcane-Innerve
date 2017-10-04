a82a6492bbd2829e4b230ea3f84436fa92ef680e
Anupriya-Singh <anupriya228@gmail.com>
2017-10-04 12:34:42 +0530

    update

--- arcane/index.php
+++ arcane/index.php
@@ -1,51 +1,11 @@
-<!DOCTYPE html>
 <html>
 <head>
-	<meta name ="author" content="">
 <title>Innerve'17</title>
 	<link rel="stylesheet" type="text/css" href="home.css">
-	    <script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
-    <script type="text/javascript" charset="utf-8">
-	document.onkeydown = function(){
-  switch (event.keyCode){
-        case 116 : //F5 button
-            event.returnValue = false;
-            event.keyCode = 0;
-            return false;
-        case 82 : //R button
-            if (event.ctrlKey){ 
-                event.returnValue = false;
-                event.keyCode = 0;
-                return false;
-            }
-		case 37 : //left arrow
-            if (event.altKey){ 
-                event.returnValue = false;
-                event.keyCode = 0;
-                return false;
-            }
-		case 39 : //Right arrow
-            if (event.altKey){ 
-                event.returnValue = false;
-                event.keyCode = 0;
-                return false;
-            }
-		case 85 : //U button
-            if (event.ctrlKey){ 
-                event.returnValue = false;
-                event.keyCode = 0;
-                return false;
-            }
-    }
-}
-window.oncontextmenu = function () {
-return false;
-}
-	window.location.hash="no-back-button";
-window.location.hash="Again-No-back-button";//again because google chrome don't insert first hash into history
-window.onhashchange=function(){window.location.hash="no-back-button";}
-</script>
+	<link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
+	    
 </head>
+
 <body>
 	<img src= "logo.png" style="width:200px; height: auto;"/>
 	<div class = "home">
@@ -63,10 +23,16 @@ window.onhashchange=function(){window.location.hash="no-back-button";}
 			<br><br>
 			<p>Date: 4th-5th October 2017</p>
 			<p>Time: 9pm-3am</p>
-			<button class = "button">
-				<a href="rules.php">Enter the hunt!</a>
-			</button>
+			<button class = "button" onClick="open()">Enter the hunt!</button>
+				<script>
+				function open()
+				{
+					window.location('rules.php');
+				}
+				</script>
+	
 		</div>
+		
 	</div>
 </body>
 </html>
